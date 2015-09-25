df9bdd6d2be0dfdd533faa16993caf89f02ed7b3
John Bowler <jwbowler@gmail.com>
2015-09-24 17:56:27 -0700

    commented camus dump read for now; reading from csv manually placed in hdfs

--- src/main/scala/com/goober/BatchBin.scala
+++ src/main/scala/com/goober/BatchBin.scala
@@ -1,48 +1,93 @@
 package com.goober
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import org.apache.log4j.{Logger, Level}
 import org.apache.spark.sql.catalyst.expressions.{Row}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
 
 object BatchBin {
 
-  def locationToBucket(x: Double, y: Double): Int = {
+  type Loc = Int
+  type Eta = Double
+
+  def locationToBucket(lng: Double, lat: Double): Option[Loc] = {
     val numBucketsX = 2
     val numBucketsY = 2
 
-    val col = Math.floor(x * numBucketsX).toInt
-    val row = Math.floor(y * numBucketsY).toInt
+    val minLng = -74.05
+    val maxLng = -73.87
+    val minLat = 40.68
+    val maxLat = 40.89
 
-    col * numBucketsX + row
+    val scaledLng = (lng - minLng) / (maxLng - minLng)
+    val scaledLat = (lat - minLat) / (maxLat - minLat)
+
+    if (scaledLng < 0 || scaledLng >= 1 || scaledLat < 0 || scaledLat >= 1) {
+      None
+    }
+
+    else {
+      val col = Math.floor(scaledLng * numBucketsX).toInt
+      val row = Math.floor(scaledLat * numBucketsY).toInt
+
+      Some(col * numBucketsX + row)
+    }
   }
 
   def main(args: Array[String]) {
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+//    val log = Logger.getRootLogger()
+
     val conf = new SparkConf().setAppName("goober")
     val sc = new SparkContext()
     val sqlc = new SQLContext(sc)
 
-    val df = sqlc.read.json("hdfs://ec2-52-89-161-16.us-west-2.compute.amazonaws.com:9000/camus/topics/m2/hourly/2015/09/22/11/*")
+//    val df = sqlc.read.json("hdfs://ec2-52-89-161-16.us-west-2.compute.amazonaws.com:9000/camus/topics/monday-test3/hourly/2015/09/24/16/*")
 
-    val normedStream = df.rdd.map(row => {
-      val msgType = row.getAs[String]("_type")
-      val rid = row.getAs[Long]("rideId")
+    val text = sc.textFile("hdfs://ec2-52-89-161-16.us-west-2.compute.amazonaws.com:9000/monday.csv")
 
-      if (msgType == "TYPE1") {
-        val locX = row.getAs[Row]("location").getAs[Double]("x")
-        val locY = row.getAs[Row]("location").getAs[Double]("y")
-        val loc = locationToBucket(locX, locY)
+//    val normedStream = df.rdd.map(row => {
+//      val locX = row.getAs[String]("longitude").toDouble
+//      val locY = row.getAs[String]("latitude").toDouble
+//      val loc = locationToBucket(locX, locY)
+//
+//      val timestampStr = row.getAs[String]("timestamp")
+//      val timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timestampStr)
+//
+//      val waitTime = row.getAs[String]("wait_time").toDouble
+//
+//      if (loc.isDefined) {
+//        Seq( (timestamp, loc, waitTime) )
+//      }
+//
+//      else {
+//        Seq.empty[(Date, Loc, Eta)]
+//      }
+//    })
 
-        (rid, (Some(loc), None))
-      }
+    val normedStream = text.flatMap(line => {
+      val words = line.split(",")
 
-      else if (msgType == "TYPE2") {
-        val eta = row.getAs[Double]("waitTime")
+      val locX = words(2).toDouble
+      val locY = words(3).toDouble
+      val loc = locationToBucket(locX, locY)
 
-        (rid, (None, Some(eta)))
+      val timestampStr = words(1)
+      val timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(timestampStr)
+
+      val waitTime = words(4).toDouble
+
+      if (loc.isDefined) {
+        Seq( (timestamp, loc.get, waitTime) )
       }
 
       else {
-        throw new RuntimeException()
+        Seq.empty[(Date, Loc, Eta)]
       }
     })
 
